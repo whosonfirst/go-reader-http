70fe634769261c11ab57a15861d8f75be84df9dd
thisisaaronland <aaron@localhost>
2022-04-20 15:16:14 -0700

    update to use optional user-agent; update vendor deps

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/whosonfirst/go-reader-http
 go 1.16
 
 require (
-	github.com/whosonfirst/go-ioutil v0.0.1
-	github.com/whosonfirst/go-reader v0.5.0
+	github.com/whosonfirst/go-ioutil v1.0.1
+	github.com/whosonfirst/go-reader v0.10.0
 )
--- http_test.go
+++ http_test.go
@@ -2,15 +2,19 @@ package reader
 
 import (
 	"context"
+	"fmt"
 	"github.com/whosonfirst/go-reader"
 	"io"
 	"io/ioutil"
+	"net/url"
 	"testing"
 )
 
 func TestHTTPReader(t *testing.T) {
 
-	reader_uri := "https://data.whosonfirst.org"
+	ua := "Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:10.0) Gecko/20100101 Firefox/10.0"
+
+	reader_uri := fmt.Sprintf("https://data.whosonfirst.org?user-agent=%s", url.QueryEscape(ua))
 	file_uri := "101/736/545/101736545.geojson"
 
 	ctx := context.Background()
